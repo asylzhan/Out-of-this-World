2536db7f178410e9fd04023c14a7d30702086ec6
Asylzhan <asylzhan13@gmail.com>
2014-11-28 18:22:28 +0600

    SpaceImage add

---
+++ Out of this world/OWSpaceImageViewController.h
@@ -0,0 +1,16 @@
+//
+//  OWSpaceImageViewController.h
+//  Out of this world
+//
+//  Created by asyl13 on 28.11.14.
+//  Copyright (c) 2014 Zhailibi Salimkhanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpaceImageViewController : UIViewController
+
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ Out of this world/OWSpaceImageViewController.m
@@ -0,0 +1,37 @@
+//
+//  OWSpaceImageViewController.m
+//  Out of this world
+//
+//  Created by asyl13 on 28.11.14.
+//  Copyright (c) 2014 Zhailibi Salimkhanov. All rights reserved.
+//
+
+#import "OWSpaceImageViewController.h"
+
+@interface OWSpaceImageViewController ()
+
+@end
+
+@implementation OWSpaceImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
